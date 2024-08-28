3da89028d277cb910ff4dac3ab620a4a3a4d0638
fedor <fedorrik1@gmail.com>
2024-08-28 00:19:56 -0700

    add to_xlsx.py

--- README.md
+++ README.md
@@ -15,3 +15,8 @@ Get cenhap specific kmers from the set of centromere assemblies
 ### Run:
 - `./count_all_kmers.sh <seq_dir> <kmer_length>`
 - <seq_dir> must contain centromere sequences (full chromosomes are too long) with cenhap assignment at the and of name after "_"
+
+### to_xlsx script:
+- collect all the cnt file from `kmers_specific_cnt` dir to one xlsx file
+- Requiers `openpyxl` and `xlsxwriter` pip libraries 
+- `python3 to_xlsx.py <path/to/kmers_specific_cnt> <output>.xlsx`
---
+++ to_xlsx.py
@@ -0,0 +1,44 @@
+import os
+import pandas as pd
+from sys import argv
+
+directory = argv[1]
+xlsx_file_path = argv[2]
+
+# Получаем список файлов .tsv в указанной директории
+tsv_files = [file for file in os.listdir(directory) if file.endswith('.cnt')]
+
+# Создаем новый объект ExcelWriter
+with pd.ExcelWriter(xlsx_file_path, engine='xlsxwriter') as writer:
+    # Проходимся по всем файлам .tsv
+    for file in tsv_files:
+        # Читаем данные из каждого файла .tsv в pandas DataFrame
+        df = pd.read_csv(os.path.join(directory, file), sep='\t')
+        # Используем имя файла (без расширения) в качестве имени листа в Excel
+        sheet_name = os.path.splitext(file)[0]
+        # Записываем данные в Excel на отдельный лист
+        df.to_excel(writer, sheet_name=sheet_name, index=False)
+
+print("xlsx created")
+
+from openpyxl import load_workbook
+
+# Укажите путь к существующему файлу Excel
+
+
+# Загружаем существующий файл Excel
+workbook = load_workbook(xlsx_file_path)
+
+# Проходимся по всем листам в книге
+for sheet in workbook.sheetnames:
+    # Получаем активный лист
+    ws = workbook[sheet]
+    # Устанавливаем ширину первой колонки до 20 (вы можете изменить это значение)
+    ws.column_dimensions['A'].width = 30
+
+# Сохраняем изменения в файл Excel
+workbook.save(xlsx_file_path)
+
+print("first column width set to 30")
+
+
